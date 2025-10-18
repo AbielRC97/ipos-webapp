5693af0f68acf12f33f953c730f95a240fdf04d3
Abiel R. Crespo <abielrc97@outlook.com>
2025-10-17 18:32:32 -0700

    Eliminar vista de privacidad

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ipos.Models;
 
 namespace ipos.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public List<Product> Productos { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        Productos = new List<Product>();
     }
 
     public void OnGet()
     {
-
+        Productos = new List<Product>
+            {
+                new Product { Id = 1, Nombre = "Taza", Precio = 50, Stock = 10 },
+                new Product { Id = 2, Nombre = "Plato", Precio = 80, Stock = 5 }
+            };
     }
 }
